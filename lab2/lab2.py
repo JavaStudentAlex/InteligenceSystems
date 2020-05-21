35141896e82ebcb57e83538a3147d95dd4fc8ac9
JavaStudentAlex <tarasik201617@gmail.com>
2020-05-21 11:53:13 +0300

    Shannon criteria changed for Kulbak.

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 
 
-
 class IEIModel:
 
     __tolerance_dist = 20
@@ -22,7 +21,7 @@ class IEIModel:
         binarized_dataset = self.__build_bin_feature_matrix(train_data)
         self.__centers = self.__build_standard_bin_classes_vectors(binarized_dataset)
         neighbours = self.__define_neighbours()
-        self.__radiuses = self.__build_optimal_classes_radiuses(binarized_dataset, neighbours)
+        self.__radiuses, self.__coeffs = self.__build_optimal_classes_radiuses(binarized_dataset, neighbours)
 
     def __build_cont_tolerance_field(self, dataset, most_freq_class):
         values_matrix = dataset.loc[dataset[self.__target] == most_freq_class, self.__features].values
@@ -83,6 +82,7 @@ class IEIModel:
 
     def __build_optimal_classes_radiuses(self, bin_dataset, neighbours):
         radiuses = dict()
+        func_eff_coefs=dict()
         for cur_class_name in self.__classes:
             nghbr_class_name = neighbours[cur_class_name]
 
@@ -90,9 +90,12 @@ class IEIModel:
             cur_class_msrs_matrix = self.__get_measures(cur_class_name, bin_dataset)
             nghbr_class_msrs_matrix = self.__get_measures(nghbr_class_name, bin_dataset)
 
-            radiuses[cur_class_name] = self.__calc_optimal_radius(cur_class_center, cur_class_msrs_matrix,
-                                                                         nghbr_class_msrs_matrix)
-        return radiuses
+            radiuses[cur_class_name], func_eff_coefs[cur_class_name] = self.__calc_optimal_radius(cur_class_center,
+                                                                                                  cur_class_msrs_matrix,
+                                                                                                  nghbr_class_msrs_matrix)
+
+        return pd.DataFrame.from_dict(radiuses, orient="index", columns=["radius"]), \
+               pd.DataFrame.from_dict(func_eff_coefs, orient="index", columns=["KFE"])
 
     def __get_measures(self, class_name, bin_dataset):
         return bin_dataset.loc[bin_dataset[self.__target] == class_name, self.__features].values
@@ -108,14 +111,14 @@ class IEIModel:
                                                                                neighbour_class_matrix)
             cases[radius] = best_func_efficiency_coof
 
-        result_radius = pd.DataFrame.from_dict(cases, orient="index").idxmax()
+        result_radius = pd.DataFrame.from_dict(cases, orient="index").idxmax().values[0]
         return result_radius, cases[result_radius]
 
     def __calc_inf_efficiency_coefficient(self, radius, goal_class_center,
                                           cur_class_matrix, nghbr_class_matrix):
         cur_in, cur_out = self.__calc_how_many_in_out_measures(radius, goal_class_center, cur_class_matrix)
         nghbr_in, nghbr_out = self.__calc_how_many_in_out_measures(radius, goal_class_center, nghbr_class_matrix)
-        return self.__calc_shannon_criteria(cur_in, cur_out, nghbr_in, nghbr_out)
+        return self.__calc_KFE_criteria(cur_in, cur_out, nghbr_in, nghbr_out)
 
     def __calc_how_many_in_out_measures(self, radius, center, measures):
         in_measures = 0
@@ -129,19 +132,39 @@ class IEIModel:
                 out_measures += 1
         return in_measures, out_measures
 
-    def __calc_shannon_criteria(self, k1, k2, k3, k4):
-        def n_log_n(k, p):
-            n = k / (k + p)
-            return n * log2(n)
-        if k1 == 0 or k4 == 0:
-            return 0
-
-        return 1 + 1/2 * (n_log_n(k1, k3) + n_log_n(k2, k4) + n_log_n(k3, k1) + n_log_n(k4, k2))
+    def __calc_KFE_criteria(self, k1, k2, k3, k4):
+        n = k1 + k2 + k3 + k4
+        r = -5
+        return 1/n * log2((2 * n + pow(10, r) - k2 - k3) / (k2 + k3 + pow(10, r))) * (n - k2 - k3)
 
     def __report(self):
-        print("Model was built with {} :".format(len(self.__classes)))
+        print("Model was built with {} classes:".format(len(self.__classes)))
         for class_name in self.__classes:
-            print("{} class with {} radius".format(class_name, self.__radiuses[class_name]))
+            print("{} class with {} radius and {} KFE".format(class_name,
+                                                              self.__radiuses.loc[class_name, "radius"],
+                                                              self.__coeffs.loc[class_name, "KFE"]))
+
+    def exam(self, dataset):
+        predicted_classes = list()
+        bin_dataset = self.__build_bin_feature_matrix(dataset)
+        for measure_idx in bin_dataset.index:
+            pred_class = self.__define_class(bin_dataset.loc[measure_idx, self.__features])
+            predicted_classes.append(pred_class)
+        return predicted_classes
+
+    def __define_class(self, feature_vector):
+        belong_func_vals = dict()
+        for class_name in self.__classes:
+            center = self.__centers.loc[class_name, self.__features]
+            radius = self.__radiuses.loc[class_name, "radius"]
+            hemming_dist = self.__calc_hemming_distance(feature_vector, center)
+            bel_func_val = 1 - hemming_dist / radius
+            if bel_func_val > 0:
+                belong_func_vals[class_name] = bel_func_val
+        if len(belong_func_vals.keys()) == 0:
+            return "no of these classes"
+        return pd.DataFrame.from_dict(belong_func_vals, orient="index").idxmax().values[0]
+
 
 
 
--- main.py
+++ main.py
@@ -14,3 +14,14 @@ train = dataset.sample(frac=0.8)
 test = dataset.drop(train.index)
 
 model = IEIModel(train, features, "class")
+
+predicted_classes = model.exam(test)
+test['predicted'] = predicted_classes
+print(test[["class", "predicted"]])
+
+
+print("\ntested\n")
+
+pred_test_classes = model.exam(train)
+train['predicted'] = pred_test_classes
+print(train[['class', 'predicted']])
